c6ddfc83aca78793f8851cd856391b19ee0fe825
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-14 17:11:24 +0100

    Fix linters 3.

--- index.js
+++ index.js
@@ -7,53 +7,53 @@ displayBooks(books);
 const button = document.getElementById('add');
 
 button.addEventListener('click', () => {
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const message = document.getElementById('error-message');
-message.textContent = '';
-console.log(books.search(title.value, author.value));
-if (title.value === '' || author.value === '') {
-    message.textContent = 'Please fill both the title and the author before adding.';
-    message.style.color = 'red';
-} else if (!books.search(title.value, author.value)) {
-    books.add(title.value, author.value);
-    displayBooks(books);
-    title.value = '';
-    author.value = '';
-    message.textContent = 'The book has been added successfully.';
-    console.log()
-    message.style.color = 'green';
-} else {
-    const message = document.getElementById('error-message');
-    message.textContent = 'The book has already been added.';
-    message.style.color = 'red';
-}
-setTimeout(() => { message.textContent = ' '; }, 5000);
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const message = document.getElementById('error-message');
+  message.textContent = '';
+  console.log(books.search(title.value, author.value));
+  if (title.value === '' || author.value === '') {
+      message.textContent = 'Please fill both the title and the author before adding.';
+      message.style.color = 'red';
+  } else if (!books.search(title.value, author.value)) {
+      books.add(title.value, author.value);
+      displayBooks(books);
+      title.value = '';
+      author.value = '';
+      message.textContent = 'The book has been added successfully.';
+      console.log()
+      message.style.color = 'green';
+  } else {
+      const message = document.getElementById('error-message');
+      message.textContent = 'The book has already been added.';
+      message.style.color = 'red';
+  }
+  setTimeout(() => { message.textContent = ' '; }, 5000);
 });
 
 const booksSection = document.getElementById('books');
 const addSection = document.getElementById('add-book');
 const contactSection = document.getElementById('contact');
 document.getElementById('menu-list').addEventListener('click', () => {
-booksSection.style.display = 'flex';
-addSection.style.display = 'none';
-contactSection.style.display = 'none';
+  booksSection.style.display = 'flex';
+  addSection.style.display = 'none';
+  contactSection.style.display = 'none';
 });
 document.getElementById('menu-add-book').addEventListener('click', () => {
-booksSection.style.display = 'none';
-addSection.style.display = 'flex';
-contactSection.style.display = 'none';
+  booksSection.style.display = 'none';
+  addSection.style.display = 'flex';
+  contactSection.style.display = 'none';
 });
 document.getElementById('menu-contact').addEventListener('click', () => {
-booksSection.style.display = 'none';
-addSection.style.display = 'none';
-contactSection.style.display = 'flex';
+  booksSection.style.display = 'none';
+  addSection.style.display = 'none';
+  contactSection.style.display = 'flex';
 });
 
 const dateContainer = document.getElementById('date-time');
 function myTimer() {
-const today = DateTime.now();
-dateContainer.textContent = `${today.toLocaleString(DateTime.DATETIME_FULL)}`;
+  const today = DateTime.now();
+  dateContainer.textContent = `${today.toLocaleString(DateTime.DATETIME_FULL)}`;
 }
 myTimer();
 setInterval(() => { myTimer(); }, 1000);
