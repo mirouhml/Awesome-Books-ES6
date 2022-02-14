6e60de75e7d0f6d159b7730a0c8831ef0842b6e2
Amar Hamlaoui <amarhamlaoui@gmail.com>
2022-02-14 17:09:51 +0100

    Fix linters 2.

--- modules/display.js
+++ modules/display.js
@@ -1,16 +1,16 @@
 export default function displayBooks(books) {
-    const booksContainer = document.getElementById('books-list');
-    books.refresh();
-    const booksList = books.getList();
-    booksContainer.innerHTML = '';
-    for (let i = 0; i < booksList.length; i += 1) {
-        const book = document.createElement('li');
-        book.innerHTML = `<h3 class="book">"${booksList[i].title}" by ${booksList[i].author}</h3>
-                        <button id="book${i}" type="button">Remove</button>`;
-        booksContainer.appendChild(book);
-        document.getElementById(`book${i}`).addEventListener('click', () => {
-        books.remove(i);
-        displayBooks(books);
-        });
-    }
+  const booksContainer = document.getElementById('books-list');
+  books.refresh();
+  const booksList = books.getList();
+  booksContainer.innerHTML = '';
+  for (let i = 0; i < booksList.length; i += 1) {
+    const book = document.createElement('li');
+    book.innerHTML = `<h3 class="book">"${booksList[i].title}" by ${booksList[i].author}</h3>
+                    <button id="book${i}" type="button">Remove</button>`;
+    booksContainer.appendChild(book);
+    document.getElementById(`book${i}`).addEventListener('click', () => {
+    books.remove(i);
+    displayBooks(books);
+    });
+  }
 }
